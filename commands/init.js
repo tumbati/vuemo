01065e809d89f838001dc5e4de26a624f4ef4afe
nejat-njonjo <nejatmhango@gmail.com>
2021-07-30 20:00:21 +0200

    move router and store

--- commands/init.js
+++ commands/init.js
@@ -2,6 +2,7 @@ const ls = require('../lib/list')
 const chalk = require('chalk')
 const fs = require('fs')
 const currentPath = process.cwd()
+const emoji = require('node-emoji')
 
 function init(...options) {
   const directories = ls('dir')
@@ -23,16 +24,48 @@ function init(...options) {
     }
   }
 
+  /**
+   * Creating core module directories
+   */
+
   const modularRootDirectories = ['core', 'modules', 'layouts']
   for (const dir of modularRootDirectories) {
-    if (!directories.includes(dir)) {
-      fs.mkdir(`${currentPath}/${dir}`, (err) => {
+    if (!srcContents.includes(dir)) {
+      fs.mkdir(`${currentPath}/src/${dir}`, (err) => {
         if (err) {
           console.log(err)
           return
         }
-        console.log(chalk.green(`Created ${dir} folder`))
+        console.log(chalk.green(`${emoji.get('file_folder')}: created ${dir} folder`))
       })
+    } else {
+      const dirName = chalk.bold.blue(dir)
+      console.log(chalk.blue(`${emoji.get('file_folder')} ${dirName} already exists`))
+    }
+  }
+
+  /**
+   * Move core folders and files into src folder
+   */
+  for (const element of srcContents) {
+    const movingContents = ['assets', 'components', 'router', 'store']
+
+    if (movingContents.includes(element)) {
+      const isDir = fs.statSync(`${currentPath}/src/${element}`).isDirectory()
+
+      if (['router', 'store'].includes(element) && isDir) {
+        // If file is router or store and is directory, move contents
+        const contents = ls({ path: `/src/${element}`, filter: null})
+        for (const content of contents) {
+          fs.renameSync(`${currentPath}/src/${element}/${content}`, `${currentPath}/src/core/${element}.js`, (err) => {
+            if (err) {
+              console.log(err)
+              return
+            }
+            console.log(chalk.green(`${emoji.get('file')}: moved ${content} as ${element}.js to core`))
+          })
+        }
+      }
     }
   }
 }
