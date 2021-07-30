140a40c89753f5d21e5184d2c956003acf931e81
nejat-njonjo <nejatmhango@gmail.com>
2021-07-30 18:40:26 +0200

    added root directories creation process

--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,7 @@
 const ls = require('../lib/list')
 const chalk = require('chalk')
+const fs = require('fs')
+const currentPath = process.cwd()
 
 function init(...options) {
   const directories = ls('dir')
@@ -10,7 +12,29 @@ function init(...options) {
     return
   }
 
+  const srcContents = ls({path: `/src`, filter: null})
+  const importantFiles = ['App.vue', 'main.js', 'router', 'store']
   
+  for (const file of importantFiles) {
+    if (!srcContents.includes(file)) {
+      const fileName = chalk.bold.green(file)
+      console.log(chalk.red(`Cannot find ${fileName} file`))
+      return
+    }
+  }
+
+  const modularRootDirectories = ['core', 'modules', 'layouts']
+  for (const dir of modularRootDirectories) {
+    if (!directories.includes(dir)) {
+      fs.mkdir(`${currentPath}/${dir}`, (err) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(chalk.green(`Created ${dir} folder`))
+      })
+    }
+  }
 }
 
 module.exports = init
