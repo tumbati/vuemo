3e5e24d48b4bbb59aacec2f32aa9af7abc13d514
nejat-njonjo <nejatmhango@gmail.com>
2021-08-01 12:53:39 +0200

    creating application module

---
+++ steps/create-core.js
@@ -0,0 +1,13 @@
+function createCore(srcDirPath) {
+  const coreDirectories = [
+    'http',
+    'helpers',
+    'guards',
+    'config',
+    'middleware',
+    'services',
+    'mixins'
+  ]
+}
+
+module.exports = createCore
