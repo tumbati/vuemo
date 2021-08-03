e697b9ce924994c7a19e189dac2b28af5e1c4db2
nejat-njonjo <nejatmhango@gmail.com>
2021-08-03 19:48:14 +0200

    added layout creating step function

--- steps/create-layout.js
+++ steps/create-layout.js
@@ -1,5 +1,21 @@
-function createLayout() {
+const { ls } = require('shelljs')
 
+/**
+ * `createLayout`
+ * 
+ * Initialized application bootstrap layout and a default layout
+ * 
+ * @param {string} layoutsDirPath Path for layouts directory
+ */
+function createLayout(layoutsDirPath) {
+  const defaultLayouts = [
+    'BootstrapLayout.vue',
+    'DefaultLayout.vue'
+  ]
+
+  const existingLayouts = ls(layoutsDirPath).filter(file => typeof file === 'string')
+
+  console.log(existingLayouts)
 }
 
 module.exports = createLayout
