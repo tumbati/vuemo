a06037cf7229befe9fe7ea45e863e5fb8d2da52b
nejat-njonjo <nejatmhango@gmail.com>
2021-08-03 20:35:30 +0200

    added app default

---
+++ docs/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <bootstrap>
+    <router-view></router-view>
+  </bootstrap>
+</template>
+
+<script>
+import Bootstrap from './layouts/Bootstrap'
+
+export default {
+  name: 'App',
+  components: {
+    Bootstrap
+  }
+}
+</script>
+
+<style scoped>
+#app, body, html {
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #151e27;
+  overflow-x: hidden;
+  font-size: 1rem;
+}
+
+button, input, textarea {
+  outline: none !important;
+  box-shadow: none !important;
+  resize: none;
+  transition: all .3s;
+}
+</style>
