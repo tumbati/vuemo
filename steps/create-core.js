a35550c8438c1f09cee1418eba177b07c9f7dc0a
nejat-njonjo <nejatmhango@gmail.com>
2021-08-03 16:29:47 +0200

    added core content creating step

--- steps/create-core.js
+++ steps/create-core.js
@@ -1,13 +1,26 @@
-function createAppCore(srcDirPath) {
-  const appCoreDirs = [
+const { ls } = require('shelljs')
+
+function createAppCore(coreDirPath) {
+  const appCoreContents = [
     'http',
     'helpers',
     'guards',
     'config',
     'middleware',
     'services',
-    'mixins'
+    'mixins',
+    'api.js',
+    'logger.js'
   ]
+  const existingContents = ls(coreDirPath).filter(content => typeof content === 'string')
+
+  for (const content of appCoreContents) {
+    if (!existingContents.includes(content)) {
+      if (content.endsWith('.js')) {
+        
+      }
+    }
+  }
 }
 
 module.exports = createAppCore
