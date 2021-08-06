aeb92760945eb4e3cd5bfddaced5ede3e0e95e98
nejat-njonjo <nejatmhango@gmail.com>
2021-08-06 21:30:59 +0200

    added route mounting of root direcotories

--- steps/index.js
+++ steps/index.js
@@ -1,8 +1,10 @@
+const mountRoot = require('./mount-root')
 const createCore = require('./create-core')
 const createLayout = require('./create-layout')
 const createSRC = require('./create-src')
 
 module.exports = {
+  mountRoot,
   createCore,
   createLayout,
   createSRC
