92036d3aedde8903735f8171e791ff1b33498d91
nejat-njonjo <nejatmhango@gmail.com>
2021-08-07 23:27:01 +0200

    added check if the process is being run in modules directory

--- commands/generate.js
+++ commands/generate.js
@@ -1,5 +1,18 @@
-const generate = (options) => {
+const currentPath = process.cwd()
+const chalk = require('chalk')
 
+const generate = () => {
+  const schema = process.argv[3].trim()
+  const schemaName = process.argv[4].trim()
+
+  switch (schema) {
+    case 'module': case 'm':
+      if (!currentPath.endsWith('/modules')) {
+        const warn = chalk.red(`Cannot find ${chalk.bold('modules')} folder`)
+        console.log(warn)
+      }
+      break;
+  }
 }
 
 module.exports = generate
