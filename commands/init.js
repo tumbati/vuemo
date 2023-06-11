b3990f0dcd73f8779ef7321dedfc4a42cef448a5
nejat-njonjo <nejatmhango@gmail.com>
2023-06-11 10:21:26 +0200

    refactor initializer

--- commands/init.js
+++ commands/init.js
@@ -1,29 +1,38 @@
-const { rm } = require('../lib')
-const currentPath = process.cwd()
+const chalk = require('chalk')
 
-const steps = require('../steps')
+function mountTypeScriptProject() {
 
-function init(flag, ...options) {
-  console.log(flag)
-  // /**
-  //  * Mount root directory structure
-  //  */
-  // steps.mountRoot(currentPath)
+}
 
-  // /**
-  //  * Remove views folder recursively from src directory
-  //  */
-  // rm(`${currentPath}/src/views`)
+function mountJavaScriptProject() {
 
-  // /**
-  //  * Create core module files
-  //  */
-  // steps.createCore(`${currentPath}/src/core`)
+}
 
-  // /**
-  //  * Create application bootstrap layout and default layout
-  //  */
-  // steps.createLayout(`${currentPath}/src/layouts`)
+function init(flag) {
+  if (!flag) {
+    mountTypeScriptProject()
+    return
+  }
+
+  const options = Object.keys(flag)
+
+  if (!options.length) {
+    mountTypeScriptProject()
+    return
+  }
+
+  if (options.includes('ts') && options.includes('js')) {
+    console.log(chalk.white(chalk.red('You can only choose one, either JavaScript or TypeScript')))
+    console.log(chalk.white(chalk.red('For example: `vuemo init --ts`')))
+    return
+  }
+
+  if (flag.ts) {
+    mountTypeScriptProject()
+    return
+  }
+
+  mountJavaScriptProject()
 }
 
 module.exports = init
