c7c07335a3a34ed1117ea9c316326e4b93617646
nejat-njonjo <nejatmhango@gmail.com>
2023-06-11 10:37:49 +0200

    refactored

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 #! /usr/bin/env node
 
-const { program } = require('commander');
-const commands = require('./commands');
+const { program } = require('commander')
+const commands = require('./commands')
 
 program.
-  version('0.0.1');
+  version('0.0.1')
 
 program
   .command('init')
   .description('Initialize a new modular architecture')
   .option('-js, --js', 'Modularize project with JavaScript')
   .option('-ts, --ts', 'Modularize project with TypeScript')
-  .action(commands.init);
+  .action(commands.init)
 
 program
   .command('generate, g')
   .description('Generates and/or modifies files based on a schematic.')
   .option('m <name>, module <name>', 'Generate module with specified name')
 
-  .action(commands.generate);
+  .action(commands.generate)
 
-program.parse(process.argv);
+program.parse(process.argv)
